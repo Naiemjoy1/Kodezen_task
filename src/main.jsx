d94947eeaf5a4b8e02cd4076135a2978925e04ce
naiemjoy1 <naiemunhasan@gmail.com>
2024-10-15 14:03:26 +0600

    reviews update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom"; // Use ReactDOM instead of createRoot
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
